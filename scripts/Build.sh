ceb87728f0caa24b8fa4b74aae7ad49d74618f26
Dawid J. Blom <dawidjblom@gmail.com>
2024-07-17 14:20:01 -0400

    Fixes The Path To The Binary When You Build The Project
    Signed-off-by: Dawid J. Blom <dawidjblom@gmail.com>

--- scripts/Build.sh
+++ scripts/Build.sh
@@ -18,7 +18,7 @@ readonly BIN_SUFFIX=.elf
 readonly CMAKE=cmake
 readonly BUILD_TYPE="Debug"
 readonly TOOLCHAIN=$(pwd)/cmake/CrossToolchain.cmake
-readonly BUILD_DIR=$(pwd)/build
+readonly BUILD_DIR=build
 readonly BIN_DIR=$BUILD_DIR/$PROJECT_NAME
 readonly SUPPORTED_BUILD_TYPES=("host" "target" "deploy")
 
@@ -55,38 +55,38 @@ function Build()
 
 function HostBuild()
 {
-    mkdir -p $BUILD_DIR/$BIN_DIR
-    $CMAKE -S . -B $BUILD_DIR/$BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                                            -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
-                                                            -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
-                                                            -DBUILD_PROJECT=ON
-    $CMAKE --build $BUILD_DIR/$BIN_DIR
+    mkdir $BIN_DIR
+    $CMAKE -S . -B $BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+                                                 -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
+                                                 -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
+                                                 -DBUILD_PROJECT=ON
+    $CMAKE --build $BIN_DIR
 }
 
 function TargetBuild()
 {
-    mkdir -p $BUILD_DIR/$BIN_DIR
-    $CMAKE -S . -B $BUILD_DIR/$BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                                            -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
-                                                            -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
-                                                            -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
-                                                            -DBUILD_PROJECT=ON
-    $CMAKE --build $BUILD_DIR/$BIN_DIR
+    mkdir -p $BIN_DIR
+    $CMAKE -S . -B $BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+                                                 -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
+                                                 -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
+                                                 -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
+                                                 -DBUILD_PROJECT=ON
+    $CMAKE --build $BIN_DIR
 }
 
 function DeployBuild()
 {
-    $CMAKE -S . -B $BUILD_DIR/$BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                                            -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
-                                                            -DCMAKE_PROJECT_VERSION=$PROJECT_VERSION_PREFIX$PROJECT_VERSION_NUM \
-                                                            -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
-                                                            -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
-                                                            -DBUILD_PROJECT_FOR_DEPLOY=ON \
-                                                            -DBUILD_PROJECT=ON
-    $CMAKE --build $BUILD_DIR/$BIN_DIR
+    $CMAKE -S . -B $BIN_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+                                                 -DCMAKE_PROJECT_NAME=$PROJECT_NAME \
+                                                 -DCMAKE_PROJECT_VERSION=$PROJECT_VERSION_PREFIX$PROJECT_VERSION_NUM \
+                                                 -DCMAKE_EXECUTABLE_SUFFIX=$BIN_SUFFIX \
+                                                 -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
+                                                 -DBUILD_PROJECT_FOR_DEPLOY=ON \
+                                                 -DBUILD_PROJECT=ON
+    $CMAKE --build $BIN_DIR
 
     DEPLOY_DIR=$BUILD_DIR/$PROJECT_NAME$PROJECT_VERSION_PREFIX$PROJECT_VERSION_NUM
     mkdir -p $DEPLOY_DIR
-    cp -r $BUILD_DIR/$BIN_DIR/$PROJECT_NAME$PROJECT_VERSION_PREFIX$PROJECT_VERSION_NUM$BIN_SUFFIX $DEPLOY_DIR
+    cp -r $BIN_DIR/$PROJECT_NAME$PROJECT_VERSION_PREFIX$PROJECT_VERSION_NUM$BIN_SUFFIX $DEPLOY_DIR
     tar cvf $DEPLOY_DIR.tar.gz $DEPLOY_DIR
 }
